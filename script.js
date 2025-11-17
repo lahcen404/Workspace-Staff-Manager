b1fd2f5b2e171ae43b78e4a3d6ebbef774287444
lahcen404 <lahcen.maskour2003@gmail.com>
2025-11-17 19:57:39 +0100

    hide and display modal form by clicking

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const addModal = document.getElementById("add-employee-modal")
+const addNewWorker = document.getElementById("open-modal-btn")
+const closeModalBtn = document.getElementById('close-modal-btn');
 
+
+addNewWorker.addEventListener('click',()=>{
+        addModal.classList.remove("hidden")
+})
+
+closeModalBtn.addEventListener("click",()=>{
+    addModal.classList.add("hidden")
+})
